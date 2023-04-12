c3c8ab1c27d911de6e7576be4df609920a091af3
Pipi <mitj88@gmail.com>
2023-04-12 13:28:19 +0300

    csv

--- CSV/Task1.py
+++ CSV/Task1.py
@@ -2,3 +2,10 @@
 Из файла Task1.csv выведите данные в формате:
 Имя - Звание
 """
+import csv
+
+if __name__ == "__main__":
+    with open("CSV/Task1.csv", mode="r+", encoding="UTF-8") as text:
+        table = csv.reader(text, delimiter=";")
+        for i in table:
+            print(i[0], i[1])

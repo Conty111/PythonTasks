92c0817b7b067d77287e08a270cdc8f18ce924e0
root <root@pipi-pupu.HP>
2023-04-24 23:19:23 +0300

    Pandas

--- Pandas/Task1.py
+++ Pandas/Task1.py
@@ -11,3 +11,24 @@
 количество билетов по классам проживания,
 количество выживших пассажиров
 """
+import pandas as pd
+
+
+dataframe = pd.read_csv("Pandas/Titanic.csv")
+fare = dataframe["Fare"]
+
+print(f"Count of passengers: {dataframe['PassengerId'].count()}")
+print(f"Mean age of passengers: {dataframe['Age'].mean()}")
+print(f"Mean cost of tickets: {fare.mean()}")
+print(f"Most expensive ticket: {fare.max()}. Most cheap ticket: {fare.min()}")
+print(f"Passenger with the most expensive ticket: {dataframe[fare==fare.max()]['Name']}\n")
+print(f"Mean age of mens: {dataframe[dataframe['Sex']=='male']['Age'].mean()}")
+print(f"Mean age of womans: {dataframe[dataframe['Sex']=='female']['Age'].mean()}")
+print(f"Mean value of survived mans: {dataframe[dataframe['Sex']=='male']['Survived'].mean()}")
+print(f"Mean value of survived womans: {dataframe[dataframe['Sex']=='female']['Survived'].mean()}")
+
+for plane_class in set(dataframe["Pclass"].values):
+    classes_now = dataframe["Pclass"] == plane_class
+    print(f"Mean cost at class {plane_class}: {dataframe[classes_now]['Fare'].mean()}")
+    print(f"Count of tickets at class {plane_class}: {dataframe[classes_now]['Ticket'].count()}")
+print(f"Count of survived: {dataframe['Survived'].count()}")

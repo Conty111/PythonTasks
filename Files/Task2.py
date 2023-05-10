f73d2f56b9461aa4291f428cb5e2739d7a6965ef
root <root@pipi-pupu.HP>
2023-05-10 13:29:39 +0300

    modified files

--- Files/Task2.py
+++ Files/Task2.py
@@ -2,3 +2,12 @@
 Напишите программу создающую еще 1 .txt файл и запишите туда строку "но у меня не получается".
 Создайте еще 1 .txt файл в котором будет объединение этого файла с файлом с предыдущего задания.
 """
+with open("second_answer.txt", "w+") as text:
+    second_sentence = "но у меня не получается"
+    text.write(second_sentence)
+
+with open("answer.txt", "r+") as text2:
+    first_sentence = text2.readline()
+        
+with open("third_answer.txt", "w+") as text3:
+    text3.write("\n".join([first_sentence, second_sentence]))
--- Project1/main.py
+++ Project1/main.py
@@ -1,13 +1,4 @@
-from analyser import get_response, make_answer, print_output
-
-
-def main():
-    print("Программа запущена!")
-    response_input = get_response()
-    while response_input:
-        answer = make_answer(response_input)
-        print_output(answer)
-        response_input = get_response()
+from analyser import main
 
 
 if __name__ == "__main__":

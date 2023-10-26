3708d0e05e682eb653e4ad1ff260539432c4ba74
Conty111 <mitj88@gmail.com>
2023-10-26 13:45:27 +0300

    Encapsulation

--- Encapsulation/Task1.py
+++ Encapsulation/Task1.py
@@ -5,3 +5,34 @@ balance и pasport приватными.
 на определенную сумму(сумма не может падать меньше 0, так что сделайте проверку).
 Создайте метод удаляющий паспортные данные с аккаунта(также по поролю).
 """
+
+class BankAccount():
+    def __init__(self, name: str, balance: float, pasport: str, password: str) -> None:
+        self._name = name
+        self.__balance = balance
+        self.__pasport= pasport
+        self.__password = password
+    
+    def get_balance(self) -> float:
+        return self.__balance
+    
+    def get_pasport(self) -> str:
+        return self.__pasport
+    
+    def set_pasport(self, pasport: str, password: str) -> bool:
+        if password == self.__password:
+            self.__pasport = pasport
+            return True
+        return False
+    
+    def del_pasport(self, password: str) -> bool:
+        if password == self.__password:
+            del self.__pasport
+            return True
+        return False
+    
+    def set_balance(self, money: float):
+        if not money < 0:
+            self.__balance = money
+        else:
+            self.__balance = 0
--- Encapsulation/Task2.py
+++ Encapsulation/Task2.py
@@ -3,3 +3,66 @@
 Создайте геттер, сеттер и делиттер чтобы можно было получить звание героя, установить звание "Победитель арены"
 в случае победы героя в битве и удалить ранк в случае поражения.
 """
+import random
+
+
+class Player():
+    def __init__(self, team: str, id_obj: int) -> None:
+        self._id_obj = id_obj
+        self.team = team
+
+class Hero(Player):
+    DEFAULT_DAMAGE = 10
+
+    def __init__(self, team: str, id_obj: int, rank: str) -> None:
+        super().__init__(id_obj=id_obj, team=team)
+        self.damage = Hero.DEFAULT_DAMAGE
+        self.__rank = rank
+
+    def level_up(self):
+        self.damage += 5
+        print(f"Level up for {self._id_obj}. Now he has {self.damage} damage")
+    
+    def get_rank(self) -> str:
+        return self.__rank
+    
+    def set_win_rank(self):
+        self.__rank = "Победитель арены"
+
+    def del_rank(self):
+        del self.__rank
+
+class Soldier(Player):
+    def __init__(self, team: str, id_obj: int) -> None:
+        super().__init__(id_obj=id_obj, team=team)
+    
+    def follow_hero(self, hero: Hero):
+        print(f"Soldier {self._id_obj} following for hero {hero._id_obj}")
+
+
+def main():
+    teams = {"red": [], "blue": []}
+    idx = 1
+    for key, val in teams.items():
+        hero = Hero(team=key, id_obj=idx)
+        val.append(hero)
+        idx += 1
+    
+    count_of_soldiers = 10
+    for i in range(count_of_soldiers):
+        s = Soldier(team=random.choice(list(teams.keys())), id_obj=(i+1))
+        teams[s.team].append(s)
+    soldiers = 0
+    win_team = ""
+    for team in teams.keys():
+        length = len(teams[team])-1
+        print(f"Team {team} has {length}")
+        if length > soldiers :
+            win_team = team
+            soldiers = length
+    
+    teams[win_team][0].level_up()
+    for s in teams[win_team][1:]:
+        s.follow_hero(teams[win_team][0])
+
+main()
